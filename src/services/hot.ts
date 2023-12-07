9bd72947f4982715445b0f0ac730fe4240cf675b
Zhoujia22 <827160082@qq.com>
2023-12-08 04:15:29 +0800

    完成HotResult类型定义

--- src/services/hot.ts
+++ src/services/hot.ts
@@ -6,6 +6,35 @@ type HotParams = {
   subType: string
 }
 
+type item = {
+  desc: string
+  id: string
+  name: string
+  picture: string
+  price: number
+}
+
+type goodItem = {
+  items: item[]
+  counts: string
+  pages: string
+  page: string
+  pageSize: string
+}
+
+type subTypes = {
+  id: string
+  title: string
+  goodsItems: goodItem[]
+}
+
+export type HotResult = {
+  title: string
+  id: string
+  bannerPicture: string
+  subTypes: subTypes[]
+}
+
 export const getHotRecommendAPI = (url: string, data?: HotParams) => {
   return http({
     method: 'GET',
