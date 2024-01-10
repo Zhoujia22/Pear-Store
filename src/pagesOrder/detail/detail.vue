b4e19bea5c95760935717c0330bb4e7586b6e09a
Zhoujia22 <827160082@qq.com>
2024-01-10 21:25:47 +0800

    完成detail页面

--- src/pagesOrder/detail/detail.vue
+++ src/pagesOrder/detail/detail.vue
@@ -1,7 +1,573 @@
+<script lang="ts" setup>
+import Guess from '@/components/Guess.vue'
+import { ref } from 'vue'
+
+const { safeAreaInsets } = uni.getSystemInfoSync()
+
+const query = defineProps<{
+  id: string
+}>()
+
+const guessRef = ref<InstanceType<typeof Guess>>()
+const scrollByBottom = () => {
+  guessRef.value?.getMore()
+}
+const popup = ref<UniHelper.UniPopupInstance>()
+const reasonList = ref([
+  '商品无货',
+  '不想要了',
+  '商品信息填错了',
+  '地址信息填写错误',
+  '商品降价',
+  '其它',
+])
+const reason = ref('')
+const onCopy = (id: string) => {
+  uni.setClipboardData({ data: id })
+}
+const pages = getCurrentPages()
+</script>
 <template>
-  <div class="detail">detail</div>
+  <view class="navbar" :style="{ paddingTop: safeAreaInsets?.top + 'px' }">
+    <view class="wrap">
+      <navigator
+        v-if="pages.length > 1"
+        open-type="navigateBack"
+        class="back icon-left"
+      ></navigator>
+      <navigator v-else url="/pages/index/index" open-type="switchTab" class="back icon-home">
+      </navigator>
+      <view class="title">订单详情</view>
+    </view>
+  </view>
+  <scroll-view scroll-y class="viewport" id="scroller" @scrolltolower="scrollByBottom">
+    <template v-if="true">
+      <view class="overview" :style="{ paddingTop: safeAreaInsets!.top + 20 + 'px' }">
+        <template v-if="true">
+          <view class="status icon-clock">等待付款</view>
+          <view class="tips">
+            <text class="money">应付金额: ¥ 99.00</text>
+            <text class="time">支付剩余</text>
+            00 时 29 分 59 秒
+          </view>
+          <view class="button">去支付</view>
+        </template>
+
+        <template v-else>
+          <view class="status"> 待付款 </view>
+          <navigator
+            class="button"
+            :url="`/pagesOrder/create/create?orderId=${query.id}`"
+            hover-class="none"
+          >
+            再次购买
+          </navigator>
+
+          <view v-if="false" class="button"> 模拟发货 </view>
+        </template>
+      </view>
+
+      <view class="shipment">
+        <view v-for="item in 1" :key="item" class="item">
+          <view class="message">
+            您已在广州市天河区黑马程序员完成取件，感谢使用菜鸟驿站，期待再次为您服务。
+          </view>
+          <view class="date"> 2023-04-14 13:14:20 </view>
+        </view>
+
+        <view class="locate">
+          <view class="user"> 张三 13333333333 </view>
+          <view class="address"> 广东省 广州市 天河区 黑马程序员 </view>
+        </view>
+      </view>
+
+      <view class="goods">
+        <view class="item">
+          <navigator
+            class="navigator"
+            v-for="item in 2"
+            :key="item"
+            :url="`/pages/goods/goods?id=${item}`"
+            hover-class="none"
+          >
+            <image
+              class="cover"
+              src="https://yanxuan-item.nosdn.127.net/c07edde1047fa1bd0b795bed136c2bb2.jpg"
+            ></image>
+            <view class="meta">
+              <view class="name ellipsis">ins风小碎花泡泡袖衬110-160cm</view>
+              <view class="type">藏青小花， 130</view>
+              <view class="price">
+                <view class="actual">
+                  <text class="symbol">¥</text>
+                  <text>99.00</text>
+                </view>
+              </view>
+              <view class="quantity">x1</view>
+            </view>
+          </navigator>
+
+          <view class="action" v-if="true">
+            <view class="button primary">申请售后</view>
+            <navigator url="" class="button"> 去评价 </navigator>
+          </view>
+        </view>
+
+        <view class="total">
+          <view class="row">
+            <view class="text">商品总价: </view>
+            <view class="symbol">99.00</view>
+          </view>
+          <view class="row">
+            <view class="text">运费: </view>
+            <view class="symbol">10.00</view>
+          </view>
+          <view class="row">
+            <view class="text">应付金额: </view>
+            <view class="symbol primary">109.00</view>
+          </view>
+        </view>
+      </view>
+
+      <view class="detail">
+        <view class="title">订单信息</view>
+        <view class="row">
+          <view class="item">
+            订单编号: {{ query.id }} <text class="copy" @tap="onCopy(query.id)">复制</text>
+          </view>
+          <view class="item">下单时间: 2023-04-14 13:14:20</view>
+        </view>
+      </view>
+
+      <Guess ref="guessRef" />
+
+      <view class="toolbar-height" :style="{ paddingBottom: safeAreaInsets?.bottom + 'px' }"></view>
+      <view class="toolbar" :style="{ paddingBottom: safeAreaInsets?.bottom + 'px' }">
+        <template v-if="true">
+          <view class="button primary"> 去支付 </view>
+          <view class="button" @tap="popup?.open?.()"> 取消订单 </view>
+        </template>
+
+        <template v-else>
+          <navigator
+            class="button secondary"
+            :url="`/pagesOrder/create/create?orderId=${query.id}`"
+            hover-class="none"
+          >
+            再次购买
+          </navigator>
+          <view class="button primary"> 确认收货 </view>
+          <view class="button"> 去评价 </view>
+          <view class="button delete"> 删除订单 </view>
+        </template>
+      </view>
+    </template>
+    <template v-else>
+      <PageSkeleton />
+    </template>
+  </scroll-view>
+  <uni-popup ref="popup" type="bottom" background-color="#fff">
+    <view class="popup-root">
+      <view class="title">订单取消</view>
+      <view class="description">
+        <view class="tips">请选择取消订单的原因：</view>
+        <view class="cell" v-for="item in reasonList" :key="item" @tap="reason = item">
+          <text class="text">{{ item }}</text>
+          <text class="icon" :class="{ checked: item === reason }"></text>
+        </view>
+      </view>
+      <view class="footer">
+        <view class="button" @tap="popup?.close?.()">取消</view>
+        <view class="button primary">确认</view>
+      </view>
+    </view>
+  </uni-popup>
 </template>
 
-<script lang="ts" setup></script>
+<style lang="scss">
+page {
+  display: flex;
+  flex-direction: column;
+  height: 100%;
+  overflow: hidden;
+}
 
-<style scoped></style>
+.navbar {
+  width: 750rpx;
+  color: #000;
+  position: fixed;
+  top: 0;
+  left: 0;
+  z-index: 9;
+  background-color: transparent;
+
+  .wrap {
+    position: relative;
+
+    .title {
+      height: 44px;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      font-size: 32rpx;
+      color: transparent;
+    }
+
+    .back {
+      position: absolute;
+      left: 0;
+      height: 44px;
+      width: 44px;
+      font-size: 44rpx;
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      color: #fff;
+    }
+  }
+}
+
+.viewport {
+  background-color: #f7f7f8;
+}
+
+.overview {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  line-height: 1;
+  padding-bottom: 30rpx;
+  color: #fff;
+  background: #2b312c;
+  .status {
+    font-size: 36rpx;
+  }
+
+  .status::before {
+    margin-right: 6rpx;
+    font-weight: 500;
+  }
+  .tips {
+    margin-top: 30rpx;
+    display: flex;
+    font-size: 14px;
+    align-items: center;
+    .money {
+      margin-right: 30rpx;
+    }
+  }
+  .button {
+    width: 260rpx;
+    height: 64rpx;
+    text-align: center;
+    line-height: 64rpx;
+    margin-top: 30rpx;
+    font-size: 28rpx;
+    color: black;
+    border-radius: 68rpx;
+    background-color: #fff;
+  }
+}
+.shipment {
+  line-height: 1.4;
+  padding: 0 20rpx;
+  margin: 20rpx 20rpx 0;
+  border-radius: 10rpx;
+  background-color: #fff;
+  .locate,
+  .item {
+    min-height: 120rpx;
+    padding: 30rpx 30rpx 25rpx 75rpx;
+    background-size: 50rpx;
+    background-repeat: no-repeat;
+    background-position: 6rpx center;
+  }
+  .locate {
+    background-image: url(https://pcapi-xiaotuxian-front-devtest.itheima.net/miniapp/images/locate.png);
+    .user {
+      font-size: 26rpx;
+      color: #444;
+    }
+    .address {
+      font-size: 24rpx;
+      color: #666;
+    }
+  }
+  .item {
+    background-image: url(https://pcapi-xiaotuxian-front-devtest.itheima.net/miniapp/images/car.png);
+    border-bottom: 1rpx solid #eee;
+    position: relative;
+    .message {
+      font-size: 26rpx;
+      color: #444;
+    }
+    .date {
+      font-size: 24rpx;
+      color: #666;
+    }
+  }
+}
+.goods {
+  margin: 20rpx 20rpx 0;
+  padding: 0 20rpx;
+  border-radius: 10rpx;
+  background-color: #fff;
+
+  .item {
+    padding: 30rpx 0;
+    border-bottom: 1rpx solid #eee;
+
+    .navigator {
+      display: flex;
+      margin: 20rpx 0;
+    }
+
+    .cover {
+      width: 170rpx;
+      height: 170rpx;
+      border-radius: 10rpx;
+      margin-right: 20rpx;
+    }
+
+    .meta {
+      flex: 1;
+      display: flex;
+      flex-direction: column;
+      justify-content: center;
+      position: relative;
+    }
+
+    .name {
+      height: 80rpx;
+      font-size: 26rpx;
+      color: #444;
+    }
+
+    .type {
+      line-height: 1.8;
+      padding: 0 15rpx;
+      margin-top: 6rpx;
+      font-size: 24rpx;
+      align-self: flex-start;
+      border-radius: 4rpx;
+      color: #888;
+      background-color: #f7f7f8;
+    }
+
+    .price {
+      display: flex;
+      margin-top: 6rpx;
+      font-size: 24rpx;
+    }
+
+    .symbol {
+      font-size: 20rpx;
+    }
+
+    .original {
+      color: #999;
+      text-decoration: line-through;
+    }
+
+    .actual {
+      margin-left: 10rpx;
+      color: #444;
+    }
+
+    .text {
+      font-size: 22rpx;
+    }
+    .quantity {
+      position: absolute;
+      bottom: 0;
+      right: 0;
+      font-size: 24rpx;
+      color: #444;
+    }
+    .action {
+      display: flex;
+      flex-direction: row-reverse;
+      justify-content: flex-start;
+      padding: 30rpx 0 0;
+      .button {
+        width: 200rpx;
+        height: 60rpx;
+        text-align: center;
+        justify-content: center;
+        line-height: 60rpx;
+        margin-left: 20rpx;
+        border-radius: 60rpx;
+        border: 1rpx solid #ccc;
+        font-size: 26rpx;
+        color: #444;
+      }
+      .primary {
+        color: #c03e46;
+        border-color: #c03e46;
+      }
+    }
+  }
+}
+.total {
+  line-height: 1;
+  font-size: 26rpx;
+  padding: 20rpx 0;
+  color: #666;
+  .row {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    padding: 10rpx 0;
+  }
+  .symbol::before {
+    content: '¥';
+    font-size: 80%;
+    margin-right: 3rpx;
+  }
+  .primary {
+    color: #cf4444;
+    font-size: 36rpx;
+  }
+}
+.detail {
+  line-height: 1;
+  padding: 30rpx 20rpx 0;
+  font-size: 26rpx;
+  color: #666;
+  border-radius: 10rpx;
+  background-color: #fff;
+  .title {
+    font-size: 30rpx;
+    color: #444;
+  }
+  .row {
+    padding: 20rpx 0;
+    .item {
+      padding: 10rpx 0;
+      display: flex;
+      align-items: center;
+    }
+    .copy {
+      border-radius: 20rpx;
+      font-size: 20rpx;
+      border: 1px solid #ccc;
+      padding: 5rpx 10rpx;
+      margin-left: 10rpx;
+    }
+  }
+}
+.toolbar-height {
+  height: 100rpx;
+  box-sizing: content-box;
+}
+.toolbar {
+  position: fixed;
+  left: 0;
+  right: 0;
+  bottom: calc(var(--window-bottom));
+  z-index: 1;
+  height: 100rpx;
+  padding: 0 20rpx;
+  display: flex;
+  align-items: center;
+  flex-direction: row-reverse;
+  border-top: 1rpx solid #ededed;
+  border-bottom: 1rpx solid #ededed;
+  background-color: #fff;
+  box-sizing: content-box;
+  .button {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+
+    width: 200rpx;
+    height: 72rpx;
+    margin-left: 15rpx;
+    font-size: 26rpx;
+    border-radius: 72rpx;
+    border: 1rpx solid #ccc;
+    color: #444;
+  }
+  .delete {
+    order: 4;
+  }
+
+  .button {
+    order: 3;
+  }
+  .secondary {
+    order: 2;
+    color: #27ba9b;
+    border-color: #27ba9b;
+  }
+
+  .primary {
+    order: 1;
+    color: #fff;
+    background-color: #27ba9b;
+  }
+}
+.popup-root {
+  padding: 30rpx 30rpx 0;
+  border-radius: 10rpx 10rpx 0 0;
+  overflow: hidden;
+  .title {
+    font-size: 30rpx;
+    text-align: center;
+    margin-bottom: 30rpx;
+  }
+  .description {
+    font-size: 28rpx;
+    padding: 0 20rpx;
+    .tips {
+      color: #444;
+      margin-bottom: 12rpx;
+    }
+
+    .cell {
+      display: flex;
+      justify-content: space-between;
+      align-items: center;
+      padding: 15rpx 0;
+      color: #666;
+    }
+    .icon::before {
+      content: '\e6cd';
+      font-family: 'erabbit' !important;
+      font-size: 38rpx;
+      color: #999;
+    }
+
+    .icon.checked::before {
+      content: '\e6cc';
+      font-size: 38rpx;
+      color: #27ba9b;
+    }
+  }
+  .footer {
+    display: flex;
+    justify-content: space-between;
+    padding: 30rpx 0 40rpx;
+    font-size: 28rpx;
+    color: #444;
+
+    .button {
+      flex: 1;
+      height: 72rpx;
+      text-align: center;
+      line-height: 72rpx;
+      margin: 0 20rpx;
+      color: #444;
+      border-radius: 72rpx;
+      border: 1rpx solid #ccc;
+    }
+
+    .primary {
+      color: #fff;
+      background-color: #27ba9b;
+      border: none;
+    }
+  }
+}
+</style>
